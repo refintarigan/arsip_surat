fc4731e6fbdd58322e6f59243305cf6e6ce1aa5a
kickymaulana <kickymaulana@gmail.com>
2025-02-21 08:24:57 +0000

    ubah halaman login

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>LOGIN</title>
+	<title>LOGIN REFIN</title>
 	<style type="text/css">
 		body{
 			font-size: 11pt;
