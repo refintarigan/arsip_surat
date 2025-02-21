8019e4f58d43c127ba25a2ddef0cb2552e26ec13
Eliza Deani <elizadeani03@gmail.com>
2025-02-21 16:00:24 +0700

    ubah jadi eliza

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>LOGIN KICKY MAULANA</title>
+	<title>LOGIN eliza</title>
 	<style type="text/css">
 		body{
 			font-size: 11pt;
